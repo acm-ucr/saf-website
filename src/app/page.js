0b1e59c864df6b13bddc9e2b201d852e7f227047
SafeDuck <schopra579@gmail.com>
2024-01-09 13:52:15 -0800

    created redbox

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import SAF from "@/components/home/SAF";
 import PhotoStrip from "@/components/home/PhotoStrip";
 import WhyJoin from "@/components/home/WhyJoin";
 import Circles from "@/components/home/get-invloved/Circles";
+import RedBox from "@/components/events/RedBox";
 
 const Home = () => {
   return (
     <div className="w-full overflow-clip flex flex-col justify-center items-center gap-10">
       <Landing />
       <SAF />
+      <RedBox />
       <PhotoStrip />
       <WhyJoin />
       <PhotoStrip />
