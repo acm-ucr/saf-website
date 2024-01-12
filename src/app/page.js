8f5b64bdae8c9ec754f11ad04b2e7e6471128001
dvuong917 <dvuongwm@gmail.com>
2024-01-12 13:05:48 -0800

    photostrip component implementation

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,17 @@ import SAF from "@/components/home/SAF";
 import PhotoStrip from "@/components/home/PhotoStrip";
 import WhyJoin from "@/components/home/WhyJoin";
 import Circles from "@/components/home/get-invloved/Circles";
+import { photoData1 } from "@/data/PhotoStrip1";
+import { photoData2 } from "@/data/PhotoStrip2";
 
 const Home = () => {
   return (
     <div className="w-full overflow-clip flex flex-col justify-center items-center gap-10">
       <Landing />
       <SAF />
-      <PhotoStrip />
+      <PhotoStrip photoData={photoData1} />
       <WhyJoin />
-      <PhotoStrip />
+      <PhotoStrip photoData={photoData2} />
       <Circles />
     </div>
   );
--- src/components/home/PhotoStrip.jsx
+++ src/components/home/PhotoStrip.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
-const PhotoStrip = () => {
-  return <div>PhotoStrip</div>;
+const PhotoStrip = ({ photoData }) => {
+  return (
+    <div className="flex flex-row justify-center w-full h-80">
+      {photoData.map((photo, index) => (
+        <Image
+          className="w-1/3"
+          key={index}
+          src={photo.image}
+          alt={photo.alt}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default PhotoStrip;
