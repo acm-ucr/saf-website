3f5442c92b89a0d904bfbc6c0ada2da2cf94a5cc
Brian Helewa <brianhelewa@Brians-MacBook-Pro-3.local>
2024-01-15 22:57:52 -0800

    Used tailwind components and added a text parameter for 'about us'

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,20 +1,28 @@
 import React from "react";
+import Image from "next/image";
 
-const Title = () => {
-  const containerHeight = "170px";
-
+const Title = ({ text }) => {
+  // Added text prop
   return (
-    <div
-      className="relative w-2/3 overflow-hidden"
-      style={{ height: containerHeight }}
-    >
-      <img
-        src="/svg/Landing.svg"
-        alt="Landing"
-        className="absolute bottom-0 left-0 w-full h-auto"
-      />
-      <div className="absolute bottom-0 left-0 right-0 flex justify-center items-center">
-        <div>Title</div>
+    <div className="relative w-2/3 overflow-hidden h-64">
+      <div className="absolute top-1 left-0 w-full h-full">
+        <Image
+          src="/svg/Landing.svg"
+          alt="Landing"
+          layout="fill" // Corrected to 'layout'
+          objectFit="cover" // Adjust as needed
+          objectPosition="bottom" // Adjust if needed
+        />
+      </div>
+      <div
+        className="absolute bottom-0 
+      left-0 w-full text-center p-2 
+      text-saf-red
+      text-4xl
+      font-bold
+      font-serif"
+      >
+        {text}
       </div>
     </div>
   );
