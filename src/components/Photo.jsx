2ba71f8c68bdfe032b6fac346cca3de82d8eb087
natalie shin <natto26@natalies-MacBook-Pro.local>
2024-01-16 14:05:36 -0800

    changed proportions of image

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -6,11 +6,11 @@ import mandala from "../../public/svg/mandala.svg";
 const Photo = () => {
   return (
     <div className="flex relative justify-center items-center w-1/2">
-      <Image src={mandala} className="absolute z-1 -left-[15%]" alt="mandala" />
-      <Image src={image1} className="w-full z-2" alt="group photo" />
+      <Image src={mandala} className="absolute z-1 -left-[-7%]" alt="mandala" />
+      <Image src={image1} className="w-2/3 z-2" alt="group photo" />
       <Image
         src={mandala}
-        className="absolute z-1 -right-[15%]"
+        className="absolute z-1 -right-[-7%]"
         alt="mandala"
       />
     </div>
