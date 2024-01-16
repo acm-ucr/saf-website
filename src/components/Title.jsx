91b14aaf1abb38c31f86981b02aebe2ab9942da4
Brian Helewa <brianhelewa@Brians-MacBook-Pro-3.local>
2024-01-15 19:10:15 -0800

    added updates to the about page. added class to className fix to Footer.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
 const Title = () => {
+  const containerHeight = "170px";
+
   return (
-    <div className="flex justify-center items-center relative h-96"> {/* Adjust height as needed */}
-      <div className="z-10">Title</div> {/* z-index for layering */}
-      <img src="/svg/Landing.svg" alt="Landing" className="absolute top-0 left-0 w-24 h-auto" /> {/* Adjust size and position as needed */}
+    <div
+      className="relative w-2/3 overflow-hidden"
+      style={{ height: containerHeight }}
+    >
+      <img
+        src="/svg/Landing.svg"
+        alt="Landing"
+        className="absolute bottom-0 left-0 w-full h-auto"
+      />
+      <div className="absolute bottom-0 left-0 right-0 flex justify-center items-center">
+        <div>Title</div>
+      </div>
     </div>
   );
 };
