8c33a838dd6590343559ee229b58c301881ac110
Ari Lee <arielleharyanto@gmail.com>
2024-01-16 13:46:58 -0800

    add to WhyJoin

--- src/components/home/WhyJoin.jsx
+++ src/components/home/WhyJoin.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const WhyJoin = () => {
-  return <div>WhyJoin</div>;
+  return (
+    <div className="px-14 py-14 bg-orange-50">
+      <div className="px-10 py-10 pe-8 bg-[#E2C69E] mr-60">
+        <header>
+          <h1 className="font-serif text-white mt-20"> WhyJoinSAF </h1>
+        </header>
+        <p className="flex font-serif">
+          Explore rich South Asian traditions, engage in community service, and
+          develop leadership skills at SAF! Join a vibrant community celebrating
+          diversity and unity. Join SAF for a unique and enriching college
+          experience!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default WhyJoin;
