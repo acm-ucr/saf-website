9736496fba90954f6e837597d367ad28430cd3ec
Ari Lee <arielleharyanto@gmail.com>
2024-01-16 14:27:35 -0800

    adjust WhyJoin

--- src/components/home/WhyJoin.jsx
+++ src/components/home/WhyJoin.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 const WhyJoin = () => {
   return (
-    <div className="px-14 py-14 bg-orange-50">
-      <div className="px-10 py-10 pe-8 bg-[#E2C69E] mr-60">
-        <header>
-          <h1 className="font-serif text-white mt-20"> WhyJoinSAF </h1>
-        </header>
-        <p className="flex font-serif">
+    <div className="w-full flex flex-row item-center justify-center bg-orange-50 px-14 py-14">
+      <div className="flex flex-col w-1/2 px-10 py-10 pe-8 bg-saf-beige-200">
+        <p className="ml-7 text-4xl font-bold font-libre text-white mt-20">
+          {" "}
+          WhyJoinSAF{" "}
+        </p>
+        <p className="w-3/4 font-lora text-xl">
           Explore rich South Asian traditions, engage in community service, and
           develop leadership skills at SAF! Join a vibrant community celebrating
           diversity and unity. Join SAF for a unique and enriching college
