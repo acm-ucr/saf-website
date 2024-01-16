cf10c57c0c520ae477c188414e587e04cd67e959
natalie shin <natto26@natalies-MacBook-Pro.local>
2024-01-16 12:43:34 -0800

    photo in about page

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import image1 from "../../public/images/image1.webp";
+import mandala from "../../public/svg/mandala.svg";
 
 const Photo = () => {
-  return <div>Photo</div>;
+  return (
+    <div className="flex relative justify-center items-center w-1/2">
+      <Image src={mandala} className="absolute z-1 -left-[15%]" alt="mandala" />
+      <Image src={image1} className="w-full z-2" alt="group photo" />
+      <Image
+        src={mandala}
+        className="absolute z-1 -right-[15%]"
+        alt="mandala"
+      />
+    </div>
+  );
 };
 
 export default Photo;
