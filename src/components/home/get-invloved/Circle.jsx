45a8493a1ad1a3582b03c40cf98ee10f3d528999
Nunu115 <trinhphdo@gmail.com>
2024-01-15 17:58:02 -0800

    added circle component

--- src/components/home/get-invloved/Circle.jsx
+++ src/components/home/get-invloved/Circle.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Image from "next/image";
+import mandala from "@/public/svg/mandala.svg";
 
-const Circle = () => {
-  return <div>Circle</div>;
+const Circle = ({ reactIcon, text }) => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="relative">
+        <Image src={mandala} alt="Mandala" className="w-full h-full" />
+        {reactIcon && (
+          <div className="absolute inset-0 flex items-center justify-center text-saf-red transform text-6xl">
+            {reactIcon}
+          </div>
+        )}
+      </div>
+      <div className="font-serif font-bold text-saf-red">{text}</div>
+    </div>
+  );
 };
 
 export default Circle;
