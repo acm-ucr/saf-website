c70378516e243258b9495ae9b1b050695aae0317
SafeDuck <schopra579@gmail.com>
2024-01-16 23:47:30 -0800

    update title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,27 +1,11 @@
 import React from "react";
 import Image from "next/image";
-
+import TitleAsset from "../../public/svg/title.svg";
 const Title = ({ text }) => {
-  // Added text prop
   return (
-    <div className="relative w-2/3 overflow-hidden h-64">
-      <div className="absolute top-1 left-0 w-full h-full">
-        <Image
-          src="/svg/Landing.svg"
-          alt="Landing"
-          layout="fill" // Corrected to 'layout'
-          objectFit="cover" // Adjust as needed
-          objectPosition="bottom" // Adjust if needed
-        />
-      </div>
-      <div
-        className="absolute bottom-0 
-      left-0 w-full text-center p-2 
-      text-saf-red
-      text-4xl
-      font-bold
-      font-serif"
-      >
+    <div className="w-2/3 relative">
+      <Image src={TitleAsset} alt="Title Svg" className="w-full " />
+      <div className="w-full absolute bottom-12 text-center p-2 text-saf-red text-4xl font-bold font-serif">
         {text}
       </div>
     </div>
