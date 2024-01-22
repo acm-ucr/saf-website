d277b38939e2083fc66a315c6a238f4ca8000a4f
randall-nguyen <rnguy137@ucr.edu>
2024-01-22 10:20:41 -0800

    week component implementation

---
+++ src/data/WeeklyEvents.js
@@ -0,0 +1,32 @@
+export const events = [
+  {
+    id: 1,
+    description: "Langar Seva with SSA",
+    day: "22",
+  },
+  {
+    id: 2,
+    description: "ch20 Boba Social",
+    day: "23",
+  },
+  {
+    id: 3,
+    description: "chai talk ; Stigma Against the LGBTQ+ in SA Community",
+    day: "24",
+  },
+  {
+    id: 4,
+    description: "Bollywood/Hip-hop Dance Workshop with Rayan Abeyratne",
+    day: "25",
+  },
+  {
+    id: 5,
+    description: "Dining hall night: Taste of South Asia",
+    day: "26",
+  },
+  {
+    id: 6,
+    description: "South Asian House Party",
+    day: "27",
+  },
+];
