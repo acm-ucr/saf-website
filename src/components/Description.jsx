71a26b00decfa122cc690ecdff3e8d4a36ffb26c
Sreeja Pagireddy <spagi@ucr.edu>
2024-01-22 10:59:26 -0800

    pushing

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Description = () => {
-  return <div>Description</div>;
+  return (
+    <>
+      <div className="text-left w-full px-14 py-14">
+        <p className="font-bold text-lg pe-8">Who We Are</p>
+        <p className="text-m break-normal break-words ">
+          SAF, at UC Riverside, is Riverside&apos;s South Asian Club. We aim to
+          serve as the umbrella for all organizations <br></br>
+          that identify as South Asian. We are politically oriented with the
+          goal of raising awarness of the diaspora <br></br> experience and
+          various social injustices that have occured in South Asia. We are also
+          socially and culturally <br></br> oriented, striving to have the South
+          Asia student population celebrate culture through festivals and
+          <br></br> inclusive religous events.
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Description;
