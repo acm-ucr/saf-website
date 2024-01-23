e14d9c5c26ee07398f0dba7f0ee143ecb63466d9
dvuong917 <dvuongwm@gmail.com>
2024-01-23 13:39:58 -0800

    404 component

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,25 +5,25 @@ import Link from "next/link";
 const NotFound = () => {
   return (
     <div className="flex flex-col w-full items-center bg-saf-beige-100">
-      <Image src={TitleAsset} alt="Title Svg" className="w-4/5" />
-      <p className="text-center text-7xl text-saf-red font-bold font-serif">
+      <Image src={TitleAsset} alt="Title Svg" className="w-4/5 transform" />
+      <p className="text-center text-8xl text-saf-red font-bold font-serif">
         404
       </p>
-      <p className="text-center text-xl text-saf-brown font-libre font-bold font-serif">
+      <p className="text-center text-3xl text-saf-brown font-libre font-bold font-serif">
         page not found
       </p>
       <Link
         as={Link}
         eventkey="1"
         href="/"
-        className="border-1 h-1/2 border-saf-brown w-1/6 text-center no-underline text-saf-brown font-libre font-serif"
+        className="border-1 border-saf-brown w-1/6 text-center no-underline text-saf-brown px-3 py-3 text-sm font-libre font-bold font-serif"
       >
         BACK TO HOME
       </Link>
       <Image
         src={TitleAsset}
         alt="Title Svg"
-        className="w-2/3 transform -scale-y-100"
+        className="w-4/5 transform -scale-y-100"
       />
     </div>
   );
