12db2573df860d290eec74114fdc1c668a12dd5f
Nunu115 <trinhphdo@gmail.com>
2024-01-23 13:22:08 -0800

    mapped thru and added links

--- src/components/home/get-invloved/Circle.jsx
+++ src/components/home/get-invloved/Circle.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import Image from "next/image";
 import mandala from "@/public/svg/mandala.svg";
+import Link from "next/link";
 
-const Circle = ({ reactIcon, text }) => {
+const Circle = ({ text, link, icon }) => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="relative">
-        <Image src={mandala} alt="Mandala" className="w-full h-full" />
-        {reactIcon && (
+    <Link href={link} style={{ textDecoration: "none" }}>
+      <div className="flex flex-col items-center">
+        <div className="relative">
+          <Image src={mandala} alt="Mandala" className="w-full h-full" />
           <div className="absolute inset-0 flex items-center justify-center text-saf-red transform text-6xl">
-            {reactIcon}
+            {icon}
           </div>
-        )}
+        </div>
+        <div className="font-serif font-bold text-saf-red">{text}</div>
       </div>
-      <div className="font-serif font-bold text-saf-red">{text}</div>
-    </div>
+    </Link>
   );
 };
 
