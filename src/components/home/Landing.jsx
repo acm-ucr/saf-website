e4743983426a3a0e70fbc202cf76a28ba9d9ca4f
javi <javi@Javis-MacBook-Pro-3.local>
2024-01-23 14:48:30 -0800

    Edit Landing.svg and finish Landing without ripple

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import Flower from "../../../public/svg/landing.svg";
-import Swirl from "../../../public/svg/ripple.svg";
+
 import Image from "next/image";
 
 const Landing = () => {
   return (
-    <div className="top-0 w-full h-auto bg-saf-beige-100 flex flex-row">
-      <div className=" relative left-[-30.3333%] ">
+    <div className="top-0 w-full h-1/6 bg-saf-beige-100 flex flex-row ">
+      <div className=" w-full relative">
         <Image src={Flower} alt="saf flower" />
       </div>
 
-      <div className=" mt-[10%] relative right-[-19%] ">
-        <Image src={Swirl} alt="saf swirl" />
+      <div className=" mt-[20%] textBox flex flex-row relative items">
+        <div className=" ml-4 relative flex flex-col">
+          <p className="relative text-center text-7xl text-saf-red text-nowrap font-bold">
+            South Asian Federation
+          </p>
+
+          <p className="relative text-3xl text-black text-right">
+            culturally, politically, and socially uniting the South Asian
+            student population at UC Riverside
+          </p>
+        </div>
+
+        <div className="right-[-20%] w-4 h-48 bg-saf-beige-200 ml-4"></div>
       </div>
-
-      <div className=" mt-[26%] right-[-20%] w-4 h-60 bg-saf-beige-200"></div>
-
-      <p className="font-size:xl bg-saf">South Asian Federation</p>
     </div>
   );
 };
