93a0208e671dfbe2daf79375f48482ded4e2e558
SafeDuck <schopra579@gmail.com>
2024-01-23 22:46:10 -0800

    fix

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,7 +7,12 @@ const page = () => {
   return (
     <div className="w-full overflow-clip flex flex-col justify-center items-center gap-10">
       <Title />
-      <Description />
+      <Description
+        title={"Who We Are"}
+        text={
+          "SAF, at UC Riverside, is Riverside's South Asian Club. We aim to serve as the umbrella for all organizations that identify as South Asian. We are politically oriented with the goal of raising awarness of the diaspora experience and various social injustices that have occured in South Asia. We are also socially and culturally oriented, striving to have the South Asia student population celebrate culture through festivals and inclusive religous events."
+        }
+      />
       <Photo />
       <Description />
     </div>
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 
-const Description = () => {
+const Description = ({ title, text }) => {
   return (
-    <>
-      <div className="text-left w-full px-14 py-14">
-        <p className="font-bold text-lg pe-8">Who We Are</p>
-        <p className="text-m break-normal break-words ">
-          SAF, at UC Riverside, is Riverside&apos;s South Asian Club. We aim to
-          serve as the umbrella for all organizations <br></br>
-          that identify as South Asian. We are politically oriented with the
-          goal of raising awarness of the diaspora <br></br> experience and
-          various social injustices that have occured in South Asia. We are also
-          socially and culturally <br></br> oriented, striving to have the South
-          Asia student population celebrate culture through festivals and
-          <br></br> inclusive religous events.
-        </p>
+    <div className="flex flex-col w-5/6 gap-2">
+      <div className="flex flex-row items-center gap-3">
+        <div className="bg-saf-beige-200 w-3 h-11" />
+        <p className="font-bold text-3xl h-full m-0">{title}</p>
       </div>
-    </>
+      <p className="text-xl">{text}</p>
+    </div>
   );
 };
 
