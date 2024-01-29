9061e6ace6edd0e77cb1575d7a0ba6997f3413ec
Brian Helewa <brianhelewa@Brians-MacBook-Pro-3.local>
2024-01-28 20:12:47 -0800

    Developed Banquet page, included: title, image, and descriptions

--- src/app/banquet/page.js
+++ src/app/banquet/page.js
@@ -1,12 +1,39 @@
 import React from "react";
 import Title from "@/components/Title";
 import Description from "@/components/Description";
+import Image from "next/image";
 
 const page = () => {
   return (
     <div className="w-full overflow-clip flex flex-col justify-center items-center gap-10">
-      <Title />
-      <Description />
+      <Title text="Banquet" />
+      <Description
+        title={"What is Banquet?"}
+        text={
+          "Banquet is our end of the year celebration! It is a time for all of us to gather together and look back at the wonderful year we have had together."
+        }
+      />
+      <div className="flex justify-center items-center w-full h-60">
+        <Image
+          src="/images/image4.webp"
+          width={500}
+          height={300}
+          objectFit="cover"
+          alt="banquet"
+        />
+      </div>
+      <div className="flex flex-col w-5/6 gap-2">
+        <p className="text-xl">
+          {
+            "The wait is over! Grab your friends and get your tickets for the most anticipated banquet of the year. Join us for an unforgettable night of music, food, and celebration! Purchase your ticket now. Anyone is able to go!"
+          }
+        </p>
+        <p className="text-xl">
+          {
+            "For those of you participating, get those applications in by Sunday, October 23rd at 11:59 p.m. for your final chance to be a part of our backstage crew. For further questions or concerns, please reach out to us on Instagram!"
+          }
+        </p>
+      </div>
     </div>
   );
 };
