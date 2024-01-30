b9153a27dd47d54eb8e522558fdb126a54ee8ec1
Sreeja Pagireddy <spagi@ucr.edu>
2024-01-30 10:08:11 -0800

    culture-show

--- src/app/culture-show/page.js
+++ src/app/culture-show/page.js
@@ -7,8 +7,13 @@ import PhotoBar1 from "../../../public/images/ImageBar1.webp";
 const page = () => {
   return (
     <div className="w-full overflow-clip flex flex-col justify-center items-center gap-10">
-      <Title />
-      <Description />
+     <Title />
+      <Description
+        title={"What is Culture Show"}
+        text={
+          "The culture show is hosted once a year typically in spring. It is a time for many of out talented members to come out and show off their skills whether that be acting, singing, or dancing to respect our culture. It is a creative way for us the spread our culture with all. "
+        }
+      />
       <PhotoStrip imageBar={PhotoBar1} />
     </div>
   );
