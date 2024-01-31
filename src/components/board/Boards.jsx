96d989c434639ed6622b6436a80f63d5d753842c
SafeDuck <schopra579@gmail.com>
2024-01-31 00:34:49 -0800

    board fix

--- src/components/board/Boards.jsx
+++ src/components/board/Boards.jsx
@@ -1,30 +1,17 @@
 import React from "react";
-import Image from "next/image";
 import { board } from "../../data/Board.js";
-import mandala from "../../../public/svg/board-mandala.svg";
-
-// centering issues/
+import Board from "./Board.jsx";
 
 const Boards = () => {
   return (
-    <div className="grid grid-cols-3 gap-x-20 gap-y-20 relative flex-col items-center">
+    <div className="grid grid-cols-3 gap-x-20 gap-y-20 flex-col items-center">
       {board.map((BOARD, index) => (
-        <div key={index} className="bg-saf-beige-100">
-          <Image src={mandala} alt="Mandala" className="w-full z-1 relative" />
-          <Image
-            className="absolute z-2 -left-[-3%] -bottom-[-11%]"
-            key={index}
-            src={BOARD.image}
-            alt={"board member image"}
-          />
-          <div className="flex relative items-center justify-center font-bold font-serif text-xl z-1 bg-saf-beige-200">
-            <div className="text-saf-red">
-              {BOARD.name}
-
-              <div className="text-black">{BOARD.position}</div>
-            </div>
-          </div>
-        </div>
+        <Board
+          position={BOARD.position}
+          name={BOARD.name}
+          image={BOARD.image}
+          key={index}
+        />
       ))}
     </div>
   );
--- src/data/Board.js
+++ src/data/Board.js
@@ -1,33 +1,38 @@
-import placeholderBoard from "../../public/images/placeholder_board.webp";
+import Ryan from "../../public/images/board/Ryan.png";
+import Riti from "../../public/images/board/Riti.png";
+import Kishan from "../../public/images/board/Kishan.png";
+import Chinmayi from "../../public/images/board/Chinmayi.png";
+import Zuha from "../../public/images/board/Zuha.png";
+import Ayesha from "../../public/images/board/Ayesha.webp";
 
 export const board = [
   {
-    image: placeholderBoard,
+    image: Ryan,
     name: "Robin Abeyratne",
     position: "Coordinator",
   },
   {
-    image: placeholderBoard,
+    image: Riti,
     name: "Riti Desai",
     position: "Representative",
   },
   {
-    image: placeholderBoard,
+    image: Kishan,
     name: "Kishan Patel",
     position: "Representative",
   },
   {
-    image: placeholderBoard,
+    image: Chinmayi,
     name: "Chinmayi",
     position: "Co-President",
   },
   {
-    image: placeholderBoard,
+    image: Zuha,
     name: "Zuha Hayat",
     position: "Director",
   },
   {
-    image: placeholderBoard,
+    image: Ayesha,
     name: "Ayesha Perrera",
     position: "Public Relations",
   },
