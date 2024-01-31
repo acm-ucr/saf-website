10f9fffdc726044bf1631b06a2b804e683431fca
SafeDuck <schopra579@gmail.com>
2024-01-30 23:36:38 -0800

    ripple on why join

--- src/components/home/WhyJoin.jsx
+++ src/components/home/WhyJoin.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import Ripple from "../../../public/svg/ripple.svg";
 
 const WhyJoin = () => {
   return (
     <div className="w-full flex flex-row item-center justify-center my-14">
-      <div className="flex flex-col w-4/5 sm:w-2/3 px-4 py-2 sm:px-10 sm:py-10 pe-2 sm:pe-8 bg-saf-beige-200">
+      <div className="flex flex-col w-4/5 relative overflow-clip sm:w-2/3 px-4 py-2 sm:px-10 sm:py-10 pe-2 sm:pe-8 bg-saf-beige-200">
+        <Image
+          src={Ripple}
+          className="w-1/2 absolute -right-[10%] -top-[20%]"
+          alt={"ripple asset svg"}
+        />
         <p className="ml-3 sm:ml-8 text-2xl sm:text-4xl font-bold font-libre text-white mt-20">
           WhyJoinSAF
         </p>
-        <p className="sm:w-3/4 font-lora text-xs sm:text-xl">
+        <p className="sm:w-3/4 font-lora text-xs sm:text-xl z-2">
           Explore rich South Asian traditions, engage in community service, and
           develop leadership skills at SAF! Join a vibrant community celebrating
           diversity and unity. Join SAF for a unique and enriching college
