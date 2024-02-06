3b86dd0e0950d46129ef55124d6886b7fca2ea5c
natalie shin <natto26@natalies-MacBook-Pro.local>
2024-02-06 00:14:16 -0800

    south asian awareness week text addition

--- src/app/awareness-week/page.js
+++ src/app/awareness-week/page.js
@@ -6,8 +6,22 @@ import Week from "@/components/awareness-week/Week";
 const page = () => {
   return (
     <div className="w-full overflow-clip flex flex-col justify-center items-center gap-10">
-      <Title />
-      <Description />
+      <Title text={"South Asian Awareness Week"} />
+      <Description
+        className="whitespace-pre"
+        title={"What is South Asian Awareness Week?"}
+        text={
+          "South Asian Awareness week is a week during which we will be hosting an event everyday from Sunday Jan 22nd til Friday Jan 27th so make sure to keep an eye out on our Instagram for each event as the individual posters for each event will be posted before the event with further details!!"
+        }
+      />
+
+      <div className="flex flex-col w-5/6 whitespace-pre">
+        <p className="text-xl">
+          For further questions or concerns, please reach out to us on
+          Instagram!
+        </p>
+      </div>
+
       <Week />
     </div>
   );
