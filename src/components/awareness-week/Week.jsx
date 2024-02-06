278d0c15187787cabe1b28a95df4b964bf49936e
dvuong917 <dvuongwm@gmail.com>
2024-02-06 13:29:20 -0800

    mobile awareness week WIP

--- src/components/awareness-week/Week.jsx
+++ src/components/awareness-week/Week.jsx
@@ -3,6 +3,34 @@ import { events } from "@/data/WeeklyEvents.js";
 
 const Week = () => {
   return (
+    <div className="w-full bg-saf-red sm:bg-blue-200 text-white">
+      <div className="text-center text-xl">SAF @ UCR presents</div>
+      <div className="font-bold py-4 sm:py-5 text-center text-4xl">
+        South Asian Awareness Week
+      </div>
+      <div className="mr-[5%] sm:mr-[16%] flex justify-center">
+        <div alt="event-numerical-day" className="font-bold text-4xl">
+          {events.map((events, index) => (
+            <div key={index} className="px-4 sm:px-20 py-3 sm:py-3">
+              <p>{events.day}</p>
+            </div>
+          ))}
+        </div>
+        <div alt="event-title">
+          {events.map((events, index) => (
+            <div key={index} className="text-sm sm:text-base py-4 sm:py-4">
+              <p>{events.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+/* REMOVE LATER
+const Week = () => {
+  return (
     <div className="w-full bg-saf-red text-white">
       <div className="text-center text-xl">SAF @ UCR presents</div>
       <div class="font-bold py-5 text-center text-4xl">
@@ -27,5 +55,6 @@ const Week = () => {
     </div>
   );
 };
+*/
 
 export default Week;
