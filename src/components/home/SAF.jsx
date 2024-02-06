2ee9db6f1894bc3410e1110d97c1f5ebaf9b7f28
Ari Lee <arielleharyanto@gmail.com>
2024-02-06 15:13:57 -0800

    animation SAF

--- src/components/home/SAF.jsx
+++ src/components/home/SAF.jsx
@@ -1,19 +1,42 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import whiteLogo from "../../../public/svg/white-logo.svg";
 import Link from "next/link";
+import { motion } from "framer-motion";
+
+const SAFanimation = {
+  hidden: { opacity: 0, y: 20 },
+  show: {
+    opacity: 0,
+    y: 0,
+  },
+};
+
+const transition = {
+  duration: 0.8,
+};
+
 const SAF = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center py-5 bg-saf-red gap-3">
       <Image
         src={whiteLogo}
         alt="SAF Logo"
-        className="sm:w-1/4 w-1/4 md:w-1/12 lg:w-1/12"
+        className="sm:w-1/4 w-1/4  md:w-1/12 lg:w-1/12"
       />
-      <p className=" text-white text-center sm:text-xl md:text-2xl lg:text-3xl font-libre font-bold font-serif">
-        {" "}
-        Who We Are
-      </p>
+
+      <motion.div
+        variants={SAFanimation}
+        transition={{ ...transition, delay: 0.4 }}
+        initial="hidden"
+        whileInView="show"
+      >
+        <p className=" text-white text-center sm:text-xl md:text-2xl lg:text-3xl font-libre font-bold font-serif">
+          {" "}
+          Who We Are
+        </p>
+      </motion.div>
       <p className=" text-white text-center sm:text-sm md:text-base lg:text-xl font-libre font-serif h-1/2 w-5/6">
         {" "}
         SAF, at UC Riverside, is Riverside’s South Asian Club. We aim to serve
@@ -24,12 +47,23 @@ const SAF = () => {
         have the South Asian student population celebrate culture through
         festivals and inclusive religious events.
       </p>
-      <Link
-        href="/"
-        className="min-w-[10rem] border-1 border-white w-1/6 text-center no-underline text-white px-3 py-3 sm:text-sm md:text-base lg:text-lg font-libre font-bold font-serif"
+      <motion.div
+        whileHover={{
+          scale: 1.1,
+          textShadow: "0px 0px 4px rgb(265,255,255)",
+        }}
+        variants={SAFanimation}
+        transition={{ delay: 0.2 }}
+        initial="hidden"
+        whileInView="show"
       >
-        Learn More
-      </Link>
+        <Link
+          href="/"
+          className="min-w-[10rem] border-1 border-white w-1/6 text-center no-underline text-white px-3 py-3 sm:text-sm md:text-base lg:text-lg font-libre font-bold font-serif"
+        >
+          Learn More
+        </Link>
+      </motion.div>
     </div>
   );
 };
