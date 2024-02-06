8c942a59c7ed20a883c3164b411177a658a35bec
Brian Helewa <brianhelewa@Brians-MacBook-Pro-3.local>
2024-02-06 13:07:22 -0800

    got the title.jsx to be responsive but the navigation bar interferes

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import TitleAsset from "../../public/svg/title.svg";
 const Title = ({ text }) => {
   return (
-    <div className="w-2/3 relative">
-      <Image src={TitleAsset} alt="Title Svg" className="w-full " />
-      <div className="w-full absolute bottom-12 text-center p-2 text-saf-red text-4xl font-bold font-serif">
+    <div className="flex justify-center w-full sm:w-full md:w-1/2 lg:1/2 relative">
+      <Image src={TitleAsset} alt="Title Svg" className="w-full" />
+      <div className="w-full sm:w-full sm:absoulte bottom-6 absolute bottom-12 text-center p-2 text-saf-red text-4xl font-bold font-serif">
         {text}
       </div>
     </div>
