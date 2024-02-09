fc10e5c77e89a5000103b166b6dcc5550e982e69
SafeDuck <schopra579@gmail.com>
2024-02-09 00:58:59 -0800

    fix

--- src/components/awareness-week/Week.jsx
+++ src/components/awareness-week/Week.jsx
@@ -3,25 +3,27 @@ import { events } from "@/data/WeeklyEvents.js";
 
 const Week = () => {
   return (
-    <div className="w-full bg-saf-red sm:bg-blue-200 text-white">
-      <div className="text-center text-xl">SAF @ UCR presents</div>
-      <div className="font-bold py-4 sm:py-5 text-center text-4xl">
-        South Asian Awareness Week
-      </div>
-      <div className="mr-[5%] sm:mr-[16%] flex justify-center">
-        <div alt="event-numerical-day" className="font-bold text-4xl">
-          {events.map((events, index) => (
-            <div key={index} className="px-4 sm:px-20 py-3 sm:py-3">
-              <p>{events.day}</p>
-            </div>
-          ))}
-        </div>
-        <div alt="event-title">
-          {events.map((events, index) => (
-            <div key={index} className="text-sm sm:text-base py-4 sm:py-4">
-              <p>{events.description}</p>
-            </div>
-          ))}
+    <div className="w-full bg-saf-red text-white flex flex-col justify-center items-center">
+      <div className="w-[90%] py-10">
+        <p className="text-center text-xl">SAF @ UCR presents</p>
+        <p className="font-bold py-4 sm:py-5 text-center text-2xl md:text-4xl">
+          South Asian Awareness Week
+        </p>
+        <div className="mr-[5%] flex flex-row justify-center items-center gap-[12%]">
+          <div alt="event-numerical-day" className="flex flex-col">
+            {events.map((events, index) => (
+              <p key={index} className="font-bold text-4xl py-3">
+                {events.day}
+              </p>
+            ))}
+          </div>
+          <div alt="event-title" className="flex flex-col">
+            {events.map((events, index) => (
+              <p key={index} className="text-sm sm:text-base py-4">
+                {events.description}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
     </div>
