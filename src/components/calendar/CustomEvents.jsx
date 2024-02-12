6b46ff3a497dfcf2cea9f9ae7f61f01926eed5b8
javi <javi@Javis-MacBook-Pro-3.local>
2024-02-12 11:28:31 -0800

    Add final calendar implemntation

--- src/components/calendar/CustomEvents.jsx
+++ src/components/calendar/CustomEvents.jsx
@@ -1,6 +1,6 @@
 const CustomEvent = ({ event }) => {
   return (
-    <div className="font-playfair text-sm flex bg-saf-beige-200 justify-start p-1">
+    <div className="font-playfair text-sm flex bg-saf-red justify-start p-1">
       <p className="whitespace-nowrap m-0">
         {!event.allDay &&
           new Date(event.start).toLocaleTimeString(navigator.language, {
