eca259eb5e885fc56cde8c288392e93f54cbbab3
randall-nguyen <rnguy137@ucr.edu>
2024-02-14 11:59:37 -0800

    implemented responsive board

--- src/components/board/Boards.jsx
+++ src/components/board/Boards.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { board } from "../../data/Board.js";
 import Board from "./Board.jsx";
+import Row from "react-bootstrap/Row";
 
 const Boards = () => {
   return (
-    <div className="grid grid-cols-3 gap-x-20 gap-y-20 flex-col items-center">
-      {board.map((BOARD, index) => (
-        <Board
-          position={BOARD.position}
-          name={BOARD.name}
-          image={BOARD.image}
-          key={index}
-        />
-      ))}
+    <div className="flex flex-col items-center justify-center -mt-10 mb-5">
+      <Row className=" items-center justify-center w-5/6">
+        {board.map((BOARD, index) => (
+          <Board
+            position={BOARD.position}
+            name={BOARD.name}
+            image={BOARD.image}
+            key={index}
+          />
+        ))}
+      </Row>
     </div>
   );
 };
