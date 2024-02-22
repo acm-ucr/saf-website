7b955d24ca1fca2824e4850c8adafb05b96fa91d
Ari Lee <arielleharyanto@gmail.com>
2024-02-21 19:05:36 -0800

    animate Get Involved

--- src/components/home/get-invloved/Circle.jsx
+++ src/components/home/get-invloved/Circle.jsx
@@ -8,7 +8,7 @@ const Circle = ({ text, link, icon }) => {
   return (
     <Col
       xs={4}
-      md={3}
+      mb={3}
       lg={2}
       className="flex flex-col items-center justify-center justify-self-center self-start"
     >
@@ -16,7 +16,7 @@ const Circle = ({ text, link, icon }) => {
         <div className="flex flex-col items-center">
           <div className="relative">
             <Image src={mandala} alt="Mandala" className="md:w-full h-full" />
-            <div className="absolute inset-0 flex items-center justify-center text-saf-red transform text-4xl md:text-7xl">
+            <div className="absolute inset-0 flex items-center justify-center text-saf-red transform text-4xl md:text-7xl lg:text-6xl xl:text-7xl">
               {icon}
             </div>
           </div>
--- src/components/home/get-invloved/GetInvolved.jsx
+++ src/components/home/get-invloved/GetInvolved.jsx
@@ -1,25 +1,46 @@
+"use client";
 import React from "react";
 import Circle from "@/components/home/get-invloved/Circle";
 import { links } from "@/data/GetInvolved.js";
 import Row from "react-bootstrap/Row";
+import { motion } from "framer-motion";
+
+const GIanimation = {
+  hidden: { opacity: 0, y: 10 },
+  show: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+const transition = {
+  duration: 0.7,
+};
 
 const GetInvolved = () => {
   return (
-    <div className="my-10 md:my-14 flex flex-col items-center justify-center">
+    <div className=" my-10 md:my-14 flex flex-col items-center justify-center">
       <p className="font-libre font-bold text-xl md:text-4xl mb-6 md:mb-14">
         Get Involved
       </p>
-      <Row className="items-center justify-center w-full md:w-screen">
-        {links &&
-          links.map((linkData, index) => (
-            <Circle
-              key={index}
-              text={linkData.text}
-              link={linkData.link}
-              icon={linkData.icon}
-            />
-          ))}
-      </Row>
+      <motion.div
+        variants={GIanimation}
+        transition={{ ...transition, delay: 0.2 }}
+        initial="hidden"
+        whileInView="show"
+      >
+        <Row className="items-center justify-center w-full md:w-screen">
+          {links &&
+            links.map((linkData, index) => (
+              <Circle
+                key={index}
+                text={linkData.text}
+                link={linkData.link}
+                icon={linkData.icon}
+              />
+            ))}
+        </Row>
+      </motion.div>
     </div>
   );
 };
