92e37a8682e54798aa43a31be7ea3709c2b6ce10
Ari Lee <arielleharyanto@gmail.com>
2024-02-21 20:08:57 -0800

    animate get involved

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -19,7 +19,7 @@ const Board = ({ position, name, image }) => {
             className="w-full h-full z-1 opacity-20"
           />
           <div className="absolute -left-[-16%] -top-[-15%] justify-center z-2 w-2/3 ">
-            <Image src={image} alt="Board" />
+            <Image src={image} alt="SAF Board Member" />
           </div>
           <div className="relative -top-[-20px] md:-top-[-5px] flex flex-col items-center justify-center font-serif font-bold text-xl ">
             <div className=" flex flex-col items-center justify-center font-serif text-saf-red inset-x-0 z-2 relative -top-[40px] text-sm md:text-2xl font-libre">
