b1c2ec59c2f06405abca2265d97ff12b57f05b1b
SafeDuck <schopra579@gmail.com>
2024-02-27 13:42:07 -0800

    add

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -6,13 +6,13 @@ import Image from "next/image";
 const Landing = () => {
   return (
     <div className="w-full min-h-screen flex flex-row items-center relative">
-      <Image src={Flower} alt="saf flower" className="hidden xl:block w-1/3" />
+      <Image src={Flower} alt="saf flower" className="hidden xl:block w-1/3 " />
       <Image src={Flowermobile} alt="saf flower" className="w-1/2 xl:hidden" />
       <div className="flex flex-row justify-end md:w-2/3 absolute md:relative">
         <Image
           src={Ripplel}
           alt="ripples image"
-          className="hidden md:block w-1/2 md:absolute -translate-y-1/2 z-0"
+          className="hidden md:block w-1/2 md:absolute -translate-y-1/2 z-0 "
         />
         <div className="flex flex-col items-end md:gap-3 w-5/6 z-8">
           <p className="text-right font-libre text-2xl md:text-6xl text-saf-red text-nowrap font-bold mt-2 mb-1">
