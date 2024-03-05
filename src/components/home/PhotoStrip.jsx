86e6c2da1b7ba02c4c9a825fb71f5eabb0a2809e
Brian Helewa <brianhelewa@Brians-MacBook-Pro-3.local>
2024-03-05 13:07:13 -0800

    animated landing

--- src/components/home/PhotoStrip.jsx
+++ src/components/home/PhotoStrip.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const PhotoStrip = ({ imageBar }) => {
   const variants = {
-    hidden: { opacity: 0, y: 50 },
+    hidden: { opacity: 1, y: 50 },
     visible: {
       opacity: 1,
       y: 0,
