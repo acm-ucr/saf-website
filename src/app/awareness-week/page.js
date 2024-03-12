771e1ea0a595b37abfbd74ba5db306a91e923668
natalie shin <natto26@natalies-MacBook-Pro.local>
2024-03-12 10:58:10 -0700

    checking over font

--- src/app/awareness-week/page.js
+++ src/app/awareness-week/page.js
@@ -13,7 +13,7 @@ const page = () => {
           "South Asian Awareness week is a week during which we will be hosting an event everyday from Sunday Jan 22nd til Friday Jan 27th so make sure to keep an eye out on our Instagram for each event as the individual posters for each event will be posted before the event with further details!!"
         }
       />
-      <p className="text-xs md:text-xl w-5/6 -mt-4">
+      <p className="text-xs md:text-xl w-5/6 -mt-4 font-lora">
         For further questions or concerns, please reach out to us on Instagram!
       </p>
       <Week />
--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -5,9 +5,11 @@ const Description = ({ title, text }) => {
     <div className="flex flex-col w-5/6 gap-2">
       <div className="flex flex-row items-center gap-3">
         <div className="bg-saf-beige-200 w-3 h-11" />
-        <p className="text-lg font-bold sm:text-3xl h-full m-0">{title}</p>
+        <p className="text-lg font-bold font-libre sm:text-3xl h-full m-0">
+          {title}
+        </p>
       </div>
-      <p className="text-xs sm:text-xl">{text}</p>
+      <p className="text-xs sm:text-xl font-lora">{text}</p>
     </div>
   );
 };
