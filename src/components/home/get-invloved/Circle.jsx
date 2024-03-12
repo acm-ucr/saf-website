6768f7bc566fd4b642f816acb9db4bc8306c8372
Brian Helewa <brianhelewa@Brians-MacBook-Pro-3.local>
2024-03-11 20:27:24 -0700

    made external links for the images

--- src/components/home/get-invloved/Circle.jsx
+++ src/components/home/get-invloved/Circle.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import mandala from "@/public/svg/mandala.svg";
-import Link from "next/link";
 import Col from "react-bootstrap/Col";
 
 const Circle = ({ text, link, icon }) => {
@@ -11,13 +10,21 @@ const Circle = ({ text, link, icon }) => {
       lg={2}
       className="flex flex-col items-center justify-center justify-self-center self-start"
     >
-      <Link href={link} className="no-underline">
+      {/* Directly use an <a> tag for external links with target="_blank" and rel="noopener noreferrer" */}
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="no-underline cursor-pointer"
+      >
         <div className="flex flex-col items-center ">
           <div className="relative group">
             <Image
               src={mandala}
               alt="Mandala"
-              className="md:w-full h-full group-hover:rotate-45 duration-300 translation-[rotate]"
+              width={100}
+              height={100} // Specify height, adjust these as needed
+              className="w-full h-full group-hover:rotate-45 duration-300"
             />
             <div className="absolute inset-0 flex items-center justify-center text-saf-red transform text-4xl md:text-7xl lg:text-6xl xl:text-7xl">
               {icon}
@@ -27,7 +34,7 @@ const Circle = ({ text, link, icon }) => {
             {text}
           </p>
         </div>
-      </Link>
+      </a>
     </Col>
   );
 };
