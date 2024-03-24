6e27a35c0e7c295af15194acf8d859f862b0ba68
Shing Hung <yhung022@ucr.edu>
2024-03-24 13:42:59 -0700

    add board animation

--- src/components/board/Boards.jsx
+++ src/components/board/Boards.jsx
@@ -1,21 +1,17 @@
-import React from "react";
 import { board } from "../../data/Board.js";
 import Board from "./Board.jsx";
-import Row from "react-bootstrap/Row";
 
 const Boards = () => {
   return (
-    <div className="flex flex-col items-center justify-center -mt-10 mb-5">
-      <Row className=" items-center justify-center w-5/6">
-        {board.map((BOARD, index) => (
-          <Board
-            position={BOARD.position}
-            name={BOARD.name}
-            image={BOARD.image}
-            key={index}
-          />
-        ))}
-      </Row>
+    <div className="grid md:grid-cols-3 md:gap-10 gap-3 grid-cols-2 pb-10">
+      {board.map((BOARD, index) => (
+        <Board
+          position={BOARD.position}
+          name={BOARD.name}
+          image={BOARD.image}
+          key={index}
+        />
+      ))}
     </div>
   );
 };
