df4c542ff56c11f64348154cf572df57601ec158
SafeDuck <schopra579@gmail.com>
2024-03-26 02:01:29 -0700

    changes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -39,18 +39,18 @@ const page = () => {
         transition={{ delay: 0.5 }}
         initial="hidden"
         whileInView="show"
-        className="hidden md:flex flex-col w-5/6 -mt-4"
+        className="flex flex-col w-5/6 -mt-4 gap-10"
       >
-        <p className="text-xs sm:text-xl font-lora">
+        <div className="text-xs sm:text-xl font-lora">
           {
             "SAF hopes to have a South Asian director in Asian Pacific Student Programs and hopes to one day have a South Asian Costa Hall center at UCR. The purpose of these two goals is to provide access and resources for South Asian students. SAF is a politically oriented student organization with the goal of raising awareness of the diaspora experience and various social injustices that have occurred in South Asia to the overall UC Riverside campus. "
           }
-        </p>
-        <p className="text-xs sm:text-xl mb-10">
+        </div>
+        <div className="text-xs sm:text-xl mb-10 font-lora">
           {
             "In addition, SAF is a social and cultural organization that strives to have the South Asian student population celebrate culture through festival inclusive religious events. It is important to have this organization on campus in order to represent the South Asian population at UCR."
           }
-        </p>
+        </div>
       </motion.div>
     </div>
   );
