92ad0cb883195ab25c533d7d151bb1e52cb46626
Shing Hung <yhung022@ucr.edu>
2024-03-27 11:52:17 -0700

    animated join

--- src/components/home/get-invloved/GetInvolved.jsx
+++ src/components/home/get-invloved/GetInvolved.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Circle from "@/components/home/get-invloved/Circle";
 import { links } from "@/data/GetInvolved.js";
-import Row from "react-bootstrap/Row";
 
 const GetInvolved = () => {
   return (
-    <div className="my-10 md:my-14 flex flex-col items-center justify-center">
+    <div className="my-10 md:my-14 flex flex-col items-center justify-center w-full">
       <p className="font-libre font-bold text-xl md:text-4xl mb-6 md:mb-14">
         Get Involved
       </p>
-      <Row className="items-center justify-center w-full md:w-screen">
+      <div className="grid md:flex md:justify-evenly md:w-full grid-cols-3 gap-3 px-5">
         {links &&
           links.map((linkData, index) => (
             <Circle
@@ -17,9 +16,10 @@ const GetInvolved = () => {
               text={linkData.text}
               link={linkData.link}
               icon={linkData.icon}
+              delay={linkData.delay}
             />
           ))}
-      </Row>
+      </div>
     </div>
   );
 };
--- src/data/GetInvolved.js
+++ src/data/GetInvolved.js
@@ -11,27 +11,32 @@ export const links = [
     text: "Discord",
     icon: <FaDiscord />,
     link: "https://discord.gg/gK9tCdzB",
+    delay: 0,
   },
 
   {
     text: "Instagram",
     icon: <RiInstagramFill />,
     link: "https://www.instagram.com/ucrsaf/?hl=en",
+    delay: 0.25,
   },
 
   {
     text: "Facebook",
     icon: <FaSquareFacebook />,
     link: "https://www.facebook.com/UCRSAF?mibextid=LQQJ4d",
+    delay: 0.5,
   },
   {
     text: "Email",
     icon: <FaEnvelope />,
     link: "mailto:safucr@gmail.com",
+    delay: 0.75,
   },
   {
     text: "Youtube",
     icon: <FaYoutube />,
     link: "https://www.youtube.com/channel/UCsrtjiMal0p_ETZYnlp6zyQ",
+    delay: 1,
   },
 ];
