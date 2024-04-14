1fec2d9e4c06d844f68c183beac59386fd805eb5
menthy-wu <wumengxuan0728@gmail.com>
2024-04-13 22:40:39 -0700

    fix carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Carousel = ({ data }) => {
   return (
     <div className="relative flex overflow-hidden w-screen scale-150 md:scale-100 mb-10">
-      <div className="flex animate-marquee justify-between w-full items-stretch">
+      <div className="flex animate-marquee justify-between w-screen items-stretch">
         {data.map((image, index) => (
           <div key={index} className="h-full flex items-stretch w-fit">
             <Image
@@ -14,7 +14,7 @@ const Carousel = ({ data }) => {
           </div>
         ))}
       </div>
-      <div className="flex absolute animate-marquee-continuation justify-between w-full h-full items-stretch">
+      <div className="flex absolute animate-marquee-continuation justify-between w-screen h-full items-stretch">
         {data.map((image, index) => (
           <div key={index} className="h-full flex items-stretch w-fit">
             <Image
